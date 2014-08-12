a41a3c7a55830c088eac1f6cb3b21fa0fb9357f4
Mike Davis <mikedavis@smartmediacreative.com>
2014-08-12 16:30:44 -0400

    Wishlist controller and view

---
+++ public/js/controllers/Wishlist.js
@@ -0,0 +1,13 @@
+(function () {
+
+  function Wishlist ($scope, DataService) {
+
+  }
+
+  Wishlist.$inject = ['$scope', 'DataService'];
+
+  angular
+    .module('app')
+    .controller('Wishlist', Wishlist);
+
+})();
